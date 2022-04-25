b6de1b64029dbc40bbe96ab104001588e7c2b42d
Martin Juhás <511735>
2022-04-25 17:05:57 +0200

    more fixes

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -5,13 +5,13 @@ import subprocess
 import flask
 
 # Input injection
-def transcode_file(request, filename):
+def transcode_file(filename):
     command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
     subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
-def foo(request, user):
+def parse_user(user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -28,6 +28,3 @@ def import_urlib_version(version):
 def index():
     module = flask.request.args.get("module")
     import_urlib_version(module)
-
-
-print(base64.b64encode(pickle.dumps(RunBinSh())))
